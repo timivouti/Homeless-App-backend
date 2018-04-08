a1ecedc55321bfc5399a37ae0af4e7bb9502a1df
Timi Voutilainen <timi_v@hotmail.com>
2018-04-08 21:10:55 +0100

    cross

--- app.js
+++ app.js
@@ -17,4 +17,10 @@ app.use((err, req, res, next) => {
   res.status(422).send({ error: err.message });
 });
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 module.exports = app;
